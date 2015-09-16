0c8bb98ec50261fc4334e640a7762e97720bee86
Chumilion <2016esison@tjhsst.edu>
2015-09-16 11:20:01 -0400

    layouts in progress 4

--- app/src/main/java/chumilion/helloworld/MainActivity.java
+++ app/src/main/java/chumilion/helloworld/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_4);
+        setContentView(R.layout.layout_7);
     }
 
     @Override
